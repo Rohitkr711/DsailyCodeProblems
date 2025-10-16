b7257334d545ba57a3458d5204acdbdc5ae86e2b
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-10-16 18:50:06 +0530

    Time: 2 ms (87.58%) | Memory: 56.7 MB (5.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,42 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int length = nums.length;
-        int[] prefixArr = new int[length]; //[1,,]
+        int[] prefixArr = new int[length]; 
         int[] suffixArr = new int[length];
 
         int preProd = 1;
-        int preTotalProd = 1;
-        for (int i = 0; i < nums.length; ++i) { //[1,2,0,4]
-            if (nums[i] != 0) {
-
-                preTotalProd *= nums[i];
-                preProd = preTotalProd / nums[i];
-                prefixArr[i] = preProd;
-            } else {
-
-                prefixArr[i] = preTotalProd;
-                preTotalProd *= nums[i];
-            }
+        for (int i = 0; i < length; ++i) { //[1,2,0,4] [1,1,2,0]
+            prefixArr[i]=preProd;
+            preProd*=nums[i];
         }
 
         int sufProd = 1;
-        int sufTotalProd = 1;
         for (int i = nums.length - 1; i >= 0; --i) { //[1,2,0,4]
-            if (nums[i] != 0) {
-
-                sufTotalProd *= nums[i];
-                sufProd = sufTotalProd / nums[i];
-                suffixArr[i] = sufProd;
-            } else {
-
-                suffixArr[i] = sufTotalProd;
-                sufTotalProd *= nums[i];
-            }
+            suffixArr[i]=sufProd;
+            sufProd*=nums[i];
         }
 
         for (int i = 0; i < length; ++i) {
-            int prod = prefixArr[i] * suffixArr[i];
-            suffixArr[i] = prod;
+            suffixArr[i] = prefixArr[i] * suffixArr[i];
 
         }
 
