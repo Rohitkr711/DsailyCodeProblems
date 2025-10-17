a93274e9f4d020938a61c0f6904b1c193545724d
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-10-17 18:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.43%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -6,19 +6,32 @@ class Solution {
         for (int i = 0; i < length; ++i) {
             countArr[arr[i]]++;
         }
-        for (int j = 0; j < length; ++j) {
-            if (countArr[0] > 0) {
-                arr[j] = 0;
-                --countArr[0];
-            } else if (countArr[1] > 0) {
-                arr[j] = 1;
-                --countArr[1];
-            } else {
-                arr[j] = 2;
-                --countArr[2];
+
+        int k=0;
+        for (int j = 0; j < 3; ++j) {
+            while(countArr[j]-->0){
+                arr[k++]=j;
             }
         }
 
         // return arr;
+
+
+        // int length = arr.length;
+        // int i=0, j=0, k=length-1;
+
+
+        // for (int j = 0; j < length; ++j) {
+        //     if (countArr[0] > 0) {
+        //         arr[j] = 0;
+        //         --countArr[0];
+        //     } else if (countArr[1] > 0) {
+        //         arr[j] = 1;
+        //         --countArr[1];
+        //     } else {
+        //         arr[j] = 2;
+        //         --countArr[2];
+        //     }
+        // }
     }
 }
