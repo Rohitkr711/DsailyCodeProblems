3b2cede88df33080f96e8477bc77179e8ab8293e
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-10-17 18:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (31.93%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,37 +1,51 @@
 class Solution {
     public void sortColors(int[] arr) {
-        int length = arr.length;
-        int[] countArr = new int[3];
+        // int length = arr.length;
+        // int[] countArr = new int[3];
 
-        for (int i = 0; i < length; ++i) {
-            countArr[arr[i]]++;
-        }
+        // for (int i = 0; i < length; ++i) {
+        //     countArr[arr[i]]++;
+        // }
 
-        int k=0;
-        for (int j = 0; j < 3; ++j) {
-            while(countArr[j]-->0){
-                arr[k++]=j;
-            }
-        }
+        // int k=0;
+        // for (int j = 0; j < 3; ++j) {
+        //     while(countArr[j]-->0){ 
+        //         arr[k++]=j; 
+        //     }
+        // }
 
         // return arr;
 
 
-        // int length = arr.length;
-        // int i=0, j=0, k=length-1;
+        int length = arr.length;
+        int i=0, j=0, k=length-1;
 
+        while(j<=k){ // [2,0,2,1,1,0]
 
-        // for (int j = 0; j < length; ++j) {
-        //     if (countArr[0] > 0) {
-        //         arr[j] = 0;
-        //         --countArr[0];
-        //     } else if (countArr[1] > 0) {
-        //         arr[j] = 1;
-        //         --countArr[1];
-        //     } else {
-        //         arr[j] = 2;
-        //         --countArr[2];
-        //     }
-        // }
+        if(arr[j]==0){
+            int temp = arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            ++i;
+            ++j;
+
+        }else if(arr[j]==2){
+            int temp = arr[j];
+            arr[j]=arr[k];
+            arr[k]=temp;
+            --k;
+
+        }else{
+            ++j;
+        }
+
+        }
+
+//    j=4
+//    i=2
+//    k=3
+//    [0,0,1,1,2,2]
+
+        
     }
 }
