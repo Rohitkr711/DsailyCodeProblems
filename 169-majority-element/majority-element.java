6587b7cb5e017789d656e5ca5dc15d67b3281d5c
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-10-18 12:35:00 +0530

    Time: 14 ms (24.73%) | Memory: 49.5 MB (87.15%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) { // [2,2,1,1,1,2,2]
-        // Approach-1
-        int length = nums.length;
 
+        // APPROACH-1 (Brute-force)  [ TC->O(n^2) & SC->O(1) ]
+        // int length = nums.length;
+
+        // for (int i = 0; i < length; ++i) {
+        //     int count = 0;
+        //     for (int j = 0; j < length; ++j) {
+        //         if (nums[i] == nums[j])
+        //             ++count;
+        //     }
+        //     if (count > length / 2)
+        //         return nums[i];
+        // }
+        // return -1;
+
+        // APPROACH-2 (using Map) [TC]
+        int length = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < length; ++i) {
-            int count = 0;
-            for (int j = 0; j < length; ++j) {
-                if (nums[i] == nums[j])
-                    ++count;
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
             }
-            if (count > length / 2)
-                return nums[i];
+
         }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue()>length/2)
+            return entry.getKey();
+            // System.out.println("Country: " + entry.getKey() + ", Capital: " + entry.getValue());
+        }
+
         return -1;
 
     }
