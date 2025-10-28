4303032b528c277e38ca63bafc6d5679f440b495
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-10-29 00:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (29.89%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -9,28 +9,41 @@ class Solution {
         // return idx;
 
         // APP-2 (using BS)
-        int high = nums.length-1;
+        // int high = nums.length-1;
+        // int low = 0;
+        // int ans = high+1;
+
+        // while(low<=high){
+        //     int mid = low + (high-low)/2;
+
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid]>target){
+        //         high = mid-1;
+        //     }
+        //     else{
+        //         low=mid+1;
+        //     }
+        // }
+        // return low;
+
+        // APP-3 (using BS, ans<->low) with opti
+        int high = nums.length - 1;
         int low = 0;
-        int ans = high+1;
+        int ans = high + 1;
 
-        while(low<=high){
-            int mid = low + (high-low)/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                high = mid-1;
-            }
-            else{
-                low=mid+1;
+            if (nums[mid] >= target) {
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
             }
         }
         return low;
-
-        // APP-3 (using BS, ans<->low)
-
-
 
     }
 }
