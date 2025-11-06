1565cdbc69be97c0f2d1253cb8069d32db81c19e
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2025-11-06 20:37:21 +0530

    Time: 1512 ms (6.15%) | Memory: 42.9 MB (9.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,40 +1,63 @@
 class Solution {
     public boolean isValid(String s) {
-        // boolean check = true;
-        List<Character> list = new ArrayList<>();
-        for (char c : s.toCharArray()) {
-            list.add(c);
 
-        }
-        System.out.println(list);
+        // Approach-1
+        // List<Character> list = new ArrayList<>();
+        // for (char c : s.toCharArray()) {
+        //     list.add(c);
 
-        for (int i = 0; i <= s.length(); ++i) { // need to go till =list.size() coz list size is shrinking
-            // if(!check)
-            // break;
-            for (int j = 0; j < list.size() - 1; j++) {
+        // }
 
-                if (list.get(j) == '(' || list.get(j) == '{' || list.get(j) == '[') {
+        // for (int i = 0; i <= s.length(); ++i) {
+        //     for (int j = 0; j < list.size() - 1; j++) {
 
-                    if (list.get(j) == '(' && list.get(j + 1) == ')' || list.get(j) == '{' && list.get(j + 1) == '}'
-                            || list.get(j) == '[' && list.get(j + 1) == ']') {
+        //         if (list.get(j) == '(' || list.get(j) == '{' || list.get(j) == '[') {
 
-                        list.remove(j + 1);
-                        list.remove(j);
+        //             if (list.get(j) == '(' && list.get(j + 1) == ')' || list.get(j) == '{' && list.get(j + 1) == '}'
+        //                     || list.get(j) == '[' && list.get(j + 1) == ']') {
+
+        //                 list.remove(j + 1); // removing j+1 first cause of indexOutOfBound
+        //                 list.remove(j);
+        //                 break;
+        //             }
+                    
+
+        //         }
+        //     }
+        // }
+        // if(list.isEmpty()){
+        //     return true;
+
+        // }else{
+        //     return false;
+        // }
+
+
+        // Approach-2
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i <= s.length(); ++i) {
+
+            for (int j = 0; j < sb.length() - 1; j++) {
+
+                if (sb.charAt(j) == '(' || sb.charAt(j) == '{' || sb.charAt(j) == '[') {
+
+                    if (sb.charAt(j) == '(' && sb.charAt(j + 1) == ')' || sb.charAt(j) == '{' && sb.charAt(j+1) == '}'
+                            || sb.charAt(j) == '[' && sb.charAt(j+1) == ']') {
+
+
+                        sb.deleteCharAt(j + 1);
+                        sb.deleteCharAt(j);
                         break;
                     }
-                    // else{
-                    //     return false;
-                    // }
 
                 }
             }
         }
-        if(list.isEmpty()){
-            return true;
 
-        }else{
-            return false;
-        }
+        return sb.isEmpty();
+            
 
     }
+
 }
